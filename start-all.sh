bb42acf25496c3b64b46cf24ffc3eb0c279074fd
tejaskenny <tejaskenny17@gmail.com>
2025-08-01 22:59:34 +0530

    deployment eks

--- start-all.sh
+++ start-all.sh
@@ -2,6 +2,7 @@
 docker-compose down -v
 docker compose  up employee-api employee-api-proxy mysql -d
 cd eks/ui
+kubectl delete all --all -n famosdots
 kubectl apply -f configmap.yaml
 kubectl apply -f deployments.yaml
 kubectl apply -f service.yaml
