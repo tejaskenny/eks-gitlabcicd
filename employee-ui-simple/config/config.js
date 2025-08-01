b9831becc50e5cc9b4ecd9bde21ee6c70aae7586
tejaskenny <tejaskenny17@gmail.com>
2025-08-01 16:08:54 +0530

    deployment eks

--- employee-ui-simple/config/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-window.APP_CONFIG = {
-  API_BASE_URL: "http://103.173.197.112:7001"
-};
-
--- zftal-employee-ui/public/config.js
+++ zftal-employee-ui/public/config.js
@@ -1,2 +1,1 @@
 const API_BASE_URL = "http://103.173.197.112:7080";
-//const API_BASE_URL = "http://employee-api:7080";
